8e2179318d72fa136b5d075c9024d41fcbf7adba
n-ken <nishiken1103@gmail.com>
2016-07-24 21:52:58 +0900

    ミニバッチ学習

--- n-ken/mps20160723.py
+++ n-ken/mps20160723.py
@@ -1,5 +1,11 @@
 import numpy as np
 
+def softmax(s):
+    '''
+    ソフトマックス関数
+    '''
+    return np.exp(s) / np.exp(s).sum()
+
 def sigmoid(s):
     '''
     sigmoid関数
@@ -89,7 +95,8 @@ if __name__ == '__main__':
     W2 = np.random.randn(n_output_2, n_output_1)
     b2 = np.random.randn(n_output_2, 1)
     # layer2 = Layer(W2, b2, relu)
-    layer2 = Layer(W2, b2, sigmoid)
+    # layer2 = Layer(W2, b2, sigmoid)
+    layer2 = Layer(W2, b2, softmax)
 
     # learning rate
     epsilon = 0.1
@@ -112,55 +119,67 @@ if __name__ == '__main__':
         X_train = X_train[idx, :]
         y_train = y_train[idx, :]
 
-        for i in tqdm(range(n_training_data)):
+        for j in range(0, X_train.shape[0], n_training_data):
 
-            # 重み行列のノルムの計算し、履歴リストに追加
-            W1_history.append(np.linalg.norm(layer1._W))
-            W2_history.append(np.linalg.norm(layer2._W))
+            Delta_W1, Delta_W2, Delta_B1, Delta_B2 = 0, 0, 0, 0
 
-            # 潤伝搬計算
-            x = X_train[i, :].reshape(n_output_0, 1)
-            y1 = layer1.propagate_forward(x)
-            y2 = layer2.propagate_forward(y1)
+            for i in tqdm(range(j, j + n_training_data)):
 
-            # 各層の(活性化関数適用後の)出力を履歴に追加
-            y1_history.append(y1)
-            y2_history.append(y2)
+                # 潤伝搬計算
+                x = X_train[i, :].reshape(n_output_0, 1)
+                y1 = layer1.propagate_forward(x)
+                y2 = layer2.propagate_forward(y1)
 
-            # ネットワークの出力
-            t = y_train[i, :].reshape(n_output_2, 1)
+                # ネットワークの出力
+                t = y_train[i, :].reshape(n_output_2, 1)
 
-            # 誤差を履歴リストに追加
-            se_history.append(se(t, y2))
+                # 誤差逆伝播
+                # delta2 = d_se(t, y2) * d_relu(y2)
+                # delta1 = layer2._W.T @ delta2 * d_relu(y1)
+                delta2 = d_se(t, y2) * d_sigmoid(y2)
+                delta1 = layer2._W.T @ delta2 * d_sigmoid(y1)
 
-            # 誤差逆伝播
-            # delta2 = d_se(t, y2) * d_relu(y2)
-            # delta1 = layer2._W.T @ delta2 * d_relu(y1)
-            delta2 = d_se(t, y2) * d_sigmoid(y2)
-            delta1 = layer2._W.T @ delta2 * d_sigmoid(y1)
+                # 各イテレーションの最初のデータを格納する
+                if i % (n_training_data) == 0:
+                    # 重み行列のノルムの計算し、履歴リストに追加
+                    W1_history.append(np.linalg.norm(layer1._W))
+                    W2_history.append(np.linalg.norm(layer2._W))
+
+                    # 各層の(活性化関数適用後の)出力を履歴に追加
+                    y1_history.append(y1)
+                    y2_history.append(y2)
+
+                    # 誤差を履歴リストに追加
+                    se_history.append(se(t, y2))
+
+                # 勾配の計算
+                Delta_W2 += delta2 @ y1.T
+                Delta_B2 += delta2
+                Delta_W1 += delta1 @ x.T
+                Delta_B1 += delta1
 
             # 第二層の重み行列について、偏微分*学習率を除算
-            Delta_W2 = delta2 @ y1.T
-            layer2._W -= epsilon * Delta_W2
-            layer2._b -= epsilon * delta2
+            layer2._W -= epsilon * Delta_W2 / n_training_data
+            layer2._b -= epsilon * Delta_B2 / n_training_data
 
             # 第一層の重み行列について、偏微分*学習率を除算
-            Delta_W1 = delta1 @ x.T
-            layer1._W -= epsilon * Delta_W1
-            layer1._b -= epsilon * delta1
+            layer1._W -= epsilon * Delta_W1 / n_training_data
+            layer1._b -= epsilon * Delta_B1 / n_training_data
 
-        # 学習に使用していないデータから100個取り出し、Accuracyを計算する
-        n_correct_prediction = 0
-        n_prediction = 0
-        for _i in np.random.choice(np.arange(n_training_data, X_train.shape[0]), 100):
-            _x = X_train[_i, :].reshape(n_output_0, 1)
-            _y1 = layer1.propagate_forward(_x)
-            _y2 = layer2.propagate_forward(_y1)
+            # 学習に使用していないデータから100個取り出し、Accuracyを計算する
+            n_correct_prediction = 0
+            n_prediction = 0
+            valid_idx = np.concatenate([np.arange(0, j),
+                                        np.arange(j + n_training_data, X_train.shape[0])])
+            for _i in np.random.choice(valid_idx, 100):
+                _x = X_train[_i, :].reshape(n_output_0, 1)
+                _y1 = layer1.propagate_forward(_x)
+                _y2 = layer2.propagate_forward(_y1)
 
-            n_prediction += 1
-            if y_train[_i, :].argmax() == _y2.argmax():
-                n_correct_prediction += 1
-        cpr_history.append(n_correct_prediction / n_prediction)
+                n_prediction += 1
+                if y_train[_i, :].argmax() == _y2.argmax():
+                    n_correct_prediction += 1
+            cpr_history.append(n_correct_prediction / n_prediction)
 
 # draw W1
 plt.figure()
